3a65620a0672a1a50e99c935b28cbcadfad3b679
Darina Kovalskaya <holtzdari@gmail.com>
2021-11-18 21:52:03 +0300

    VK mini app initialized

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import bridge from '@vkontakte/vk-bridge';
-import { View, ScreenSpinner, AdaptivityProvider, AppRoot } from '@vkontakte/vkui';
+import { View, ScreenSpinner, AdaptivityProvider, AppRoot, Snackbar, Avatar } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
+import { Icon24Error } from '@vkontakte/icons';
 
 import Home from './panels/Home';
-import Persik from './panels/Persik';
+import Intro from './panels/Intro';
+
+const ROUTES = { 
+    HOME: 'home',
+	INTRO: 'intro'
+};
+
+const STORAGE_KEYS = {
+	STATUS: 'status',
+}
 
 const App = () => {
-	const [activePanel, setActivePanel] = useState('home');
+	const [activePanel, setActivePanel] = useState(ROUTES.INTRO);
 	const [fetchedUser, setUser] = useState(null);
 	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+	const [UserHasSeenIntro, setUserHasSeenIntro] = useState(false);
+	const [Snackbar, setSnackbar] = useState(false);
 
 	useEffect(() => {
 		bridge.subscribe(({ detail: { type, data }}) => {
@@ -21,6 +33,38 @@ const App = () => {
 		});
 		async function fetchData() {
 			const user = await bridge.send('VKWebAppGetUserInfo');
+			const storageData = await bridge.send('VKWebAppStorageGet', {
+				keys: Object.values(STORAGE_KEYS)
+			 });
+			console.log(storageData);
+			const data = {};
+			storageData.keys.forEach( ({ key, value }) => {
+				try {
+					data[key] = value ? JSON.parse(value) : {};
+					switch(key) {
+                        case STORAGE_KEYS.STATUS:
+							if (data[key].hasSeenIntro) {
+								setActivePanel(ROUTES.HOME);
+								setUserHasSeenIntro(true);
+							}
+							break;
+					    default:
+							break;
+					}
+				} catch(error){
+					setSnackbar(<Snackbar
+						layout='vertical'
+						onClose={() => setSnackbar(null)}
+						before={<Avatar size={24} style={{ backgroundColor: 'var(--dynamic-red)' }}
+						><Icon24Error fill='#fff' width='14' height='14' /></Avatar>}
+						duration={900}
+					>
+						Проблема с получением данных из Storage
+					</Snackbar>)
+				}
+			})
+			
+
 			setUser(user);
 			setPopout(null);
 		}
@@ -31,12 +75,33 @@ const App = () => {
 		setActivePanel(e.currentTarget.dataset.to);
 	};
 
+	const viewIntro = async function () {
+		try {
+            await bridge.send('VKWebAppStorageSet', {
+				key: STORAGE_KEYS.STATUS,
+				value: JSON.stringify({
+					hasSeenIntro: true
+				})
+			});
+		} catch (error) {
+			setSnackbar(<Snackbar
+				layout='vertical'
+				onClose={() => setSnackbar(null)}
+				before={<Avatar size={24} style={{ backgroundColor: 'var(--dynamic-red)' }}
+				><Icon24Error fill='#fff' width='14' height='14' /></Avatar>}
+				duration={900}
+			>
+				Проблема с отправкой данных в Storage
+			</Snackbar>)
+		}
+	} 
+
 	return (
 		<AdaptivityProvider>
 			<AppRoot>
 				<View activePanel={activePanel} popout={popout}>
-					<Home id='home' fetchedUser={fetchedUser} go={go} />
-					<Persik id='persik' go={go} />
+					<Home id={ ROUTES.HOME } fetchedUser={fetchedUser} go={go} snackbarError={Snackbar}/>
+					<Intro id={ ROUTES.INTRO } go={go} snackbarError={Snackbar}/>
 				</View>
 			</AppRoot>
 		</AdaptivityProvider>
